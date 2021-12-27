98facc0968657724d42454f193a294b9dfe6a6c7
techvysta <sumedhabaranwal@gmail.com>
2021-12-27 23:41:09 +0530

    section 9 start

--- demo/revents/src/features/sandbox/Sandbox.jsx
+++ demo/revents/src/features/sandbox/Sandbox.jsx
@@ -3,20 +3,24 @@ import {  useSelector, useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { decrement, increment} from '../sandbox/testReducer';
 import { openModal } from '../../app/common/modals/modalReducer';
+import TestPlaceInput from './TestPlaceInput';
 
 export default function Sandbox() {
     const dispatch = useDispatch();
     const data = useSelector((state) => state.test.data);
+    const { loading } = useSelector( (state) => state.async );
 
  return (
    <>
      <h1>Testing 123</h1>
      <h3>The data is: {data} </h3>
       <Button 
+      loading={loading}
       onClick={() => dispatch(increment(20))} 
       content='Increment' 
       color='green' />
       <Button 
+      loading={loading}
       onClick={() => dispatch(decrement(10))} 
       content='Decrement' 
       color='red' />
@@ -26,7 +30,9 @@ export default function Sandbox() {
       } 
       content='Open Modal' 
       color='teal' />
-     
+     <div style={{marginTop: 15}}>
+       < TestPlaceInput />
+     </div>
    </>
  );
 }
